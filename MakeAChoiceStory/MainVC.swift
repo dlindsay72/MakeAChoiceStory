222c96e9c78d5678c6c6439eef1870e1e66f6641
Dan Lindsay <dlindsay72@hotmail.com>
2017-02-06 13:42:47 -0500

    Added text field to get users name. Extended the UITextFieldDelegate in our MainVC and added Noitification observers so out text field animates up or down, depending on if first touched or after name is entered

--- MakeAChoiceStory/MainVC.swift
+++ MakeAChoiceStory/MainVC.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class MainVC: UIViewController {
+    
+    @IBOutlet weak var textFieldBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var nameTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let story = Page(story: .touchDown)
-        story.firstChoice = (title: "someTitle", page: Page(story: .droid))
+        NotificationCenter.default.addObserver(self, selector: #selector(MainVC.keyboardWillShow(_:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(MainVC.keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,13 +27,71 @@ class MainVC: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "startAdventure" {
-            guard let pageController = segue.destination as? PageController else {
-                return
+            
+            do {
+                if let name = nameTextField.text {
+                    if name == "" {
+                        throw AdventureError.nameNotProvided
+                    } else {
+                        guard let pageController = segue.destination as? PageController else {
+                            return
+                        }
+                        pageController.page = Adventure.story(withName: name)
+
+                    }
+                }
+            } catch AdventureError.nameNotProvided {
+                let alertController = UIAlertController(title: "Name Not Provided", message: "Provide a name to start the story", preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(action)
+                
+                present(alertController, animated: true, completion: nil)
+            } catch let error {
+                fatalError("\(error.localizedDescription)")
             }
-            pageController.page = Adventure.story
         }
     }
-
-
+    
+    func keyboardWillShow(_ notification: Notification) {
+        if let info = notification.userInfo, let keyboardFrame = info[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let frame = keyboardFrame.cgRectValue
+            textFieldBottomConstraint.constant = frame.size.height + 10
+            
+            UIView.animate(withDuration: 0.8) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    func keyboardWillHide(_ notification: Notification) {
+        textFieldBottomConstraint.constant = 40
+        
+        UIView.animate(withDuration: 0.8) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
 }
+
+extension MainVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
 
